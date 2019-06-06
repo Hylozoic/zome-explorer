10db92dc5b3873f36f856a4fb756fd44ba37da28
Robbie Carlton <robbie.carlton@gmail.com>
2019-06-06 12:40:17 -0600

    Save instance id in session Also update title

--- src/App/App.js
+++ src/App/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import './App.css'
 import useHolochainConnection from '../utils/useHolochainConnection'
 import { name, description, zomes } from '../utils/parsedDNA'
@@ -7,9 +7,18 @@ import Results from '../Results'
 
 export default function App() {
   const callZomeRef = useHolochainConnection(process.env.REACT_APP_ZOME_WEBSOCKET_URL)
-  const [instanceId, setInstanceId] = useState('')
+  const [instanceId, setInstanceId] = useState(sessionStorage.getItem('instanceId'))
   const storedHistory = sessionStorage.getItem('history')    
   const [history, setHistory] = useState((storedHistory && JSON.parse(storedHistory)) || [])  
+
+  const setInstanceIdSession = value => {
+    sessionStorage.setItem('instanceId', value)
+    setInstanceId(value)
+  } 
+
+  useEffect(() => {
+    document.title = `Zome Explorer - ${name}`
+  })
 
   const callZome = zomeName => funcName => async params => {
     const result = await callZomeRef.current(instanceId, zomeName, funcName)(params)
@@ -24,27 +33,23 @@ export default function App() {
     return callRecord
   }
 
-  const leftColumn = <div>
-    <div className='dna-name'>{name}</div>
-    <div className='dna-description'>{description}</div>      
-    <div>
-      <label className='input-label'>Instance Id</label>
-      <input value={instanceId} onChange={({ target: { value }}) => setInstanceId(value)} />
-    </div>
-    <ZomeList callZome={callZome} zomes={zomes} />
-  </div>
-
-  const rightColumn = <Results history={history} />
-
   return (
     <div className='app'>
       <div className='column-wrapper'>
         <div className='column'>
-          {leftColumn}
+          <div>
+            <div className='dna-name'>{name}</div>
+            <div className='dna-description'>{description}</div>      
+            <div>
+              <label className='input-label'>Instance Id</label>
+              <input value={instanceId} onChange={({ target: { value }}) => setInstanceIdSession(value)} />
+            </div>
+            <ZomeList callZome={callZome} zomes={zomes} />
+          </div>
         </div>
         <div className='separator' />
         <div className='column'>
-          {rightColumn}
+          <Results history={history} />
         </div>
       </div>
     </div>
