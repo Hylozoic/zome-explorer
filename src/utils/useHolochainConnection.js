822efd01af59759dddb94bc62880bf9cb94ce074
Robbie Carlton <robbie.carlton@gmail.com>
2019-06-10 19:27:55 -0600

    keep retrying to connect to connductor

--- src/utils/useHolochainConnection.js
+++ src/utils/useHolochainConnection.js
@@ -7,8 +7,13 @@ export default function useHolochainConnection (url) {
   useEffect(() => {
     var connection
     async function connectToConductor () {
-      connection = await connect({url})
-      callZomeRef.current = connection.callZome  
+      connection = await connect({url, wsClient: {max_reconnects: 0, reconnect_interval: 2500}})
+
+      const successMessage = '🎉 Successfully connected to Holochain!'
+      console.log(successMessage)
+      connection.ws.on('open', () => console.log(successMessage))
+
+      callZomeRef.current = connection.callZome
     }
     connectToConductor()
     return () => {
