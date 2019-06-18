147cf52181b8b1357790c5de9de8e4580ee44cad
Robbie Carlton <robbie.carlton@gmail.com>
2019-06-18 12:55:36 -0600

    use @hylozoic/react-holochain-hooks add connection status

--- src/App/App.js
+++ src/App/App.js
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from 'react'
 import './App.css'
-import useHolochainConnection from '../utils/useHolochainConnection'
+import useHolochainConnection from '@hylozoic/react-holochain-hooks'
 import { name, description, zomes } from '../utils/parsedDNA'
 import ZomeList from '../ZomeList'
 import Results from '../Results'
 
 export default function App() {
-  const callZomeRef = useHolochainConnection(process.env.REACT_APP_ZOME_WEBSOCKET_URL)
+  const [ready, setReady] = useState(false)
+  const { callZomeRef } = useHolochainConnection(process.env.REACT_APP_ZOME_WEBSOCKET_URL, {}, setReady)
   const [instanceId, setInstanceId] = useState(sessionStorage.getItem('instanceId') || "")
   const storedHistory = sessionStorage.getItem('history')    
   const [history, setHistory] = useState((storedHistory && JSON.parse(storedHistory)) || [])  
@@ -40,9 +41,14 @@ export default function App() {
           <div>
             <div className='dna-name'>{name}</div>
             <div className='dna-description'>{description}</div>      
-            <div>
+            <div className='instance-input'>
               <label className='input-label'>Instance Id</label>
               <input value={instanceId} onChange={({ target: { value }}) => setInstanceIdSession(value)} />
+            </div>
+            <div className='status'>
+              Status:&nbsp;
+              {ready && <span className='connected'>Connected</span>}
+              {!ready && <span className='disconnected'>Disconnected</span>}              
             </div>
             <ZomeList callZome={callZome} zomes={zomes} />
           </div>
